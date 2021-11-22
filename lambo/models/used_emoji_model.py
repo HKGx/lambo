22b3057f9eaf3e6a93702ed4df72a00103712578
HKGx <19597269+HKGx@users.noreply.github.com>
2021-11-22 20:02:19 +0100

    used_emoj_model: delete id field as it's created by default

--- lambo/models/used_emoji_model.py
+++ lambo/models/used_emoji_model.py
@@ -4,7 +4,6 @@ from tortoise import fields
 
 
 class UsedEmojiModel(Model):
-    id = fields.IntField(pk=True)
     emoji = fields.CharField(max_length=64, index=True)
     timestamp = fields.DatetimeField(auto_now_add=True)
     used_by = fields.CharField(max_length=22)  # user id
