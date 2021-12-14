52d430ffab22abebeda0aa0c60b0e9bae707331b
HKGx <19597269+HKGx@users.noreply.github.com>
2021-12-14 16:22:19 +0100

    cogs.moderation_utils: add new cog with role creation utilities

---
+++ lambo/cogs/moderation_utils.py
@@ -0,0 +1,37 @@
+import discord
+from discord.ext.commands import Cog, Context, command, group
+from lambo.custom_client import CustomClient
+
+
+class ModerationUtilsCog(Cog):
+    bot: CustomClient
+
+    def __init__(self, bot: CustomClient):
+        self.bot = bot
+
+    @group(name="role", invoke_without_command=False)
+    async def role(self, ctx: Context):
+        pass
+
+    @role.command(name="create")
+    async def role_create(self, ctx: Context, name: str, after: discord.Role = None):
+        guild: discord.Guild = ctx.guild  # type: ignore
+        if after is None:
+            after = guild.default_role
+        assert after is not None, "after is None"
+        await ctx.send(f"Creating role {name} after {after.name}")
+        role: discord.Role = await guild.create_role(name=name)
+        async with ctx.typing():
+            await role.edit(position=after.position)
+            await ctx.reply(f"Created role {role.name} with id {role.id}")
+
+    @role.command(name="move")
+    async def move_role(self, ctx: Context, role: discord.Role, after: discord.Role):
+        await ctx.send(f"Moving role {role.name} after {after.name}")
+        async with ctx.typing():
+            await role.edit(position=after.position)
+            await ctx.reply(f"Moved role {role.name} with id {role.id}")
+
+
+def setup(bot: CustomClient):
+    bot.add_cog(ModerationUtilsCog(bot))
