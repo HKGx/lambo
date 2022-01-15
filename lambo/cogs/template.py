de86babff2876396f25189515ad8dd52591597eb
HKGx <19597269+HKGx@users.noreply.github.com>
2022-01-16 00:21:30 +0100

    cogs: add template cog for simpler creation

---
+++ lambo/cogs/template.py
@@ -0,0 +1,15 @@
+import discord
+from discord.ext.commands import Cog, Context, command
+from discord.utils import escape_markdown
+from lambo.custom_client import CustomClient
+
+
+class TemplateCog(Cog, name="Template"):
+    bot: CustomClient
+
+    def __init__(self, bot: CustomClient) -> None:
+        self.bot = bot
+
+
+def setup(bot: CustomClient):
+    bot.add_cog(TemplateCog(bot))
