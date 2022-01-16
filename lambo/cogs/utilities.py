f39bcff524a3c61824d25de018ca4475a64d0929
HKGx <19597269+HKGx@users.noreply.github.com>
2022-01-16 03:32:46 +0100

    utilities: inrole should operate on flags

--- lambo/cogs/utilities.py
+++ lambo/cogs/utilities.py
@@ -2,11 +2,17 @@ from textwrap import dedent
 from tokenize import triple_quoted
 
 import discord
-from discord.ext.commands import Cog, Context, command
+from discord.ext.commands import Cog, Context, command, FlagConverter, flag
 from discord.ext.pages import PageGroup, Paginator
 from discord.utils import escape_markdown
 from lambo.custom_client import CustomClient
 from lambo.utils import FuzzyRoleConverter
+
+
+class InroleFlags(FlagConverter):
+    per_page: int = flag(name="per_page", aliases=[
+                         "pp", "perpage"], default=20)
+    with_ids: bool = flag(name="with_ids", aliases=["ids"], default=True)
 
 
 class UtilitiesCog(Cog, name="Utilities"):
@@ -24,21 +30,21 @@ class UtilitiesCog(Cog, name="Utilities"):
         await ctx.send(f"{str_users}")
 
     @command("inrole")
-    async def inrole(self, ctx: Context, _role: FuzzyRoleConverter, per_page: int = 20, with_ids: bool = True) -> None:
-        if per_page not in range(1, 30):
+    async def inrole(self, ctx: Context, _role: FuzzyRoleConverter, *, flags: InroleFlags) -> None:
+        if flags.per_page not in range(1, 30):
             await ctx.reply("Please enter a number between 1 and 30.")
             return
         role: discord.Role = _role  # type: ignore
         members = []
         for idx, member in enumerate(role.members):
             ordering = f"{(idx + 1):02}. "
-            id_str = f"{member.id} " if with_ids else ""
+            id_str = f"{member.id} " if flags.with_ids else ""
             members.append(f"{ordering}{id_str}{member}")
         # Join every per_page members in one list
-        members = ["\n".join(members[i:i + per_page])
-                   for i in range(0, len(members), per_page)]
+        members = ["\n".join(members[i:i + flags.per_page])
+                   for i in range(0, len(members), flags.per_page)]
         # Prepend every list with the custom message
-        id_str = f" ({role.id})" if with_ids else ""
+        id_str = f" ({role.id})" if flags.with_ids else ""
 
         prefix = quoted(
             f"Members in role `{role.name}`{id_str}.\n"
