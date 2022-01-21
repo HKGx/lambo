f95b25365083da3550419fba4014b99c0f6fa3de
HKGx <19597269+HKGx@users.noreply.github.com>
2022-01-22 00:12:59 +0100

    fix check in inrole command

--- lambo/cogs/utilities.py
+++ lambo/cogs/utilities.py
@@ -39,7 +39,7 @@ class UtilitiesCog(Cog, name="Utilities"):
     async def inrole(
         self, ctx: Context, _role: FuzzyRoleConverter, *, flags: InroleFlags
     ) -> None:
-        if 1 <= flags.per_page <= 30:
+        if not 1 <= flags.per_page <= 30:
             await ctx.reply("Please enter a number between 1 and 30.")
             return
         role: discord.Role = _role  # type: ignore
