3ddbe86cd7bccd8ca9ce076d9f8eb24a145f4629
HKGx <19597269+HKGx@users.noreply.github.com>
2022-02-11 08:23:04 +0100

    reformat and add role_overlap

--- lambo/cogs/moderation_utils.py
+++ lambo/cogs/moderation_utils.py
@@ -1,8 +1,7 @@
 import typing
 
 import discord
-from discord.ext.commands import (Cog, Context, FlagConverter, group,
-                                  has_permissions)
+from discord.ext.commands import Cog, Context, FlagConverter, group, has_permissions
 from lambo.custom_client import CustomClient
 from lambo.utils import FuzzyRoleConverter
 
@@ -21,6 +20,18 @@ class ModerationUtilsCog(Cog, name="Moderation Utilities"):
     @group(name="role", invoke_without_command=False)
     async def role(self, ctx: Context):
         pass
+
+    @role.command(name="overlap")
+    async def role_overlap(
+        self,
+        ctx: Context,
+        first_role_: FuzzyRoleConverter,
+        second_role_: FuzzyRoleConverter,
+    ):
+        first_role: discord.Role = first_role_  # type: ignore
+        second_role: discord.Role = second_role_  # type: ignore
+        members_overlap = set(first_role.members) & set(second_role.members)
+        await ctx.reply(f"There are {len(members_overlap)} members in both roles.")
 
     @role.command(name="id")
     async def role_id(self, ctx: Context, _role: FuzzyRoleConverter):
@@ -55,7 +66,9 @@ class ModerationUtilsCog(Cog, name="Moderation Utilities"):
     async def channel_permissions(self, ctx: Context):
         pass
 
-    def set_channel_overwrites(self, overwrites: discord.PermissionOverwrite, permissions: PermissionsFor) -> discord.PermissionOverwrite:
+    def set_channel_overwrites(
+        self, overwrites: discord.PermissionOverwrite, permissions: PermissionsFor
+    ) -> discord.PermissionOverwrite:
         """
         SIDE EFFECTS!
         """
@@ -64,41 +77,65 @@ class ModerationUtilsCog(Cog, name="Moderation Utilities"):
                 overwrites._set(perm, value)
         return overwrites
 
-    def check_bot_perms_for_channel(self, ctx: Context, channel: discord.abc.GuildChannel, permission: discord.flags.flag_value) -> bool:
+    def check_bot_perms_for_channel(
+        self,
+        ctx: Context,
+        channel: discord.abc.GuildChannel,
+        permission: discord.flags.flag_value,
+    ) -> bool:
         perm = discord.Permissions(permission.flag)
         return channel.permissions_for(ctx.guild.me) >= perm
 
     @channel_permissions.command(name="addfor")
-    async def channel_permissions_add(self,
-                                      ctx: Context,
-                                      channel: typing.Union[discord.TextChannel, discord.CategoryChannel, discord.VoiceChannel, discord.StageChannel],
-                                      member: discord.Member,
-                                      *,
-                                      permissions: PermissionsFor):
+    async def channel_permissions_add(
+        self,
+        ctx: Context,
+        channel: typing.Union[
+            discord.TextChannel,
+            discord.CategoryChannel,
+            discord.VoiceChannel,
+            discord.StageChannel,
+        ],
+        member: discord.Member,
+        *,
+        permissions: PermissionsFor,
+    ):
         assert isinstance(
-            ctx.author, discord.Member), "ctx.author is not a discord.Member"
+            ctx.author, discord.Member
+        ), "ctx.author is not a discord.Member"
         if not channel.permissions_for(ctx.author).manage_permissions:
-            await ctx.send("You do not have permission to manage permissions for this channel.")
+            await ctx.send(
+                "You do not have permission to manage permissions for this channel."
+            )
             return
-        await ctx.send(f"Adding permissions for {member.name} in {channel}. It might take a while...")
+        await ctx.send(
+            f"Adding permissions for {member.name} in {channel}. It might take a while..."
+        )
         ctx.typing()
         permissions_set_for: set[str] = set()
         permissions_lacking_for: set[str] = set()
-        if not self.check_bot_perms_for_channel(ctx, channel, discord.Permissions.manage_permissions):
+        if not self.check_bot_perms_for_channel(
+            ctx, channel, discord.Permissions.manage_permissions
+        ):
             permissions_lacking_for.add(channel.mention)
         else:
             overwrites: discord.PermissionOverwrite = channel.overwrites_for(
-                member)  # type: ignore
+                member  # type: ignore
+            )
             self.set_channel_overwrites(overwrites, permissions)
             try:
                 await channel.set_permissions(member, overwrite=overwrites)
                 permissions_set_for.add(channel.mention)
             except Exception as e:
-                await ctx.send(f"Failed to set permissions for {member.name} in {channel} : {e}")
+                await ctx.send(
+                    f"Failed to set permissions for {member.name} in {channel} : {e}"
+                )
                 permissions_lacking_for.add(channel.mention)
         if isinstance(channel, discord.CategoryChannel):
             for subchannel in channel.channels:
-                if not self.check_bot_perms_for_channel(ctx, channel, discord.Permissions.manage_permissions):
+                if not self.check_bot_perms_for_channel(
+                    ctx, channel, discord.Permissions.manage_permissions
+                ):
                     permissions_lacking_for.add(subchannel.mention)
                     continue
                 overwrites = subchannel.overwrites_for(member)  # type: ignore
@@ -107,11 +144,15 @@ class ModerationUtilsCog(Cog, name="Moderation Utilities"):
                     await subchannel.set_permissions(member, overwrite=overwrites)
                     permissions_set_for.add(subchannel.mention)
                 except Exception as e:
-                    await ctx.send(f"Failed to set permissions for {member.name} in {subchannel.name} : {e}")
+                    await ctx.send(
+                        f"Failed to set permissions for {member.name} in {subchannel.name} : {e}"
+                    )
                     permissions_lacking_for.add(subchannel.mention)
 
-        await ctx.reply(f"Set permissions for {member.name} in {', '.join(permissions_set_for)}\n"
-                        f"Couldn't set permissions for {', '.join(permissions_lacking_for)}")
+        await ctx.reply(
+            f"Set permissions for {member.name} in {', '.join(permissions_set_for)}\n"
+            f"Couldn't set permissions for {', '.join(permissions_lacking_for)}"
+        )
 
 
 def setup(bot: CustomClient):
