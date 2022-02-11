f89fb36889f26d31aed438c05d1f7bc3e066b34b
HKGx <19597269+HKGx@users.noreply.github.com>
2022-02-11 08:22:48 +0100

    reformat utilities

--- lambo/cogs/utilities.py
+++ lambo/cogs/utilities.py
@@ -11,8 +11,7 @@ from lambo.utils import FuzzyRoleConverter
 
 
 class InroleFlags(FlagConverter):
-    per_page: int = flag(name="per_page", aliases=[
-                         "pp", "perpage"], default=20)
+    per_page: int = flag(name="per_page", aliases=["pp", "perpage"], default=20)
     with_ids: bool = flag(name="with_ids", aliases=["ids"], default=True)
 
     page: int = flag(name="page", default=1)
@@ -25,15 +24,21 @@ class UtilitiesCog(Cog, name="Utilities"):
         self.bot = bot
 
     @command("mentioned")
-    async def mentioned(self, ctx: Context, message: discord.Message, with_ids: bool = False) -> None:
-        users = [escape_markdown(str(user)) +
-                 (f" ({user.id})" if with_ids else "") for user in message.mentions]
+    async def mentioned(
+        self, ctx: Context, message: discord.Message, with_ids: bool = False
+    ) -> None:
+        users = [
+            escape_markdown(str(user)) + (f" ({user.id})" if with_ids else "")
+            for user in message.mentions
+        ]
         new_line = "\n"
         str_users = f"```{new_line.join(users)}```"
         await ctx.send(f"{str_users}")
 
     @command("inrole")
-    async def inrole(self, ctx: Context, _role: FuzzyRoleConverter, *, flags: InroleFlags) -> None:
+    async def inrole(
+        self, ctx: Context, _role: FuzzyRoleConverter, *, flags: InroleFlags
+    ) -> None:
         if not (1 <= flags.per_page <= 30):
             await ctx.reply("Please enter a number between 1 and 30.")
             return
@@ -45,8 +50,10 @@ class UtilitiesCog(Cog, name="Utilities"):
             id_str = f"{member.id} " if flags.with_ids else ""
             members.append(f"{ordering}{id_str}{member}")
         # Join every per_page members in one list
-        members_pages = ["\n".join(members[i:i + flags.per_page])
-                         for i in range(0, len(members), flags.per_page)]
+        members_pages = [
+            "\n".join(members[i : i + flags.per_page])
+            for i in range(0, len(members), flags.per_page)
+        ]
         # Prepend every list with the custom message
         id_str = f" ({role.id})" if flags.with_ids else ""
 
@@ -54,15 +61,17 @@ class UtilitiesCog(Cog, name="Utilities"):
             f"Members in role `{role.name}`{id_str}.\n"
             f"Total members: {len(role.members)}.\n"
         )
-        length_disclaimer = f"Showing only first 1000 members.\n" if len(
-            role.members) > 1000 else ""
+        length_disclaimer = (
+            f"Showing only first 1000 members.\n" if len(role.members) > 1000 else ""
+        )
 
         prefix += length_disclaimer
         prefix = quoted(prefix)
-        members_pages = [f"{prefix}\n{codized(page)}"
-                         for page in members_pages]
+        members_pages = [f"{prefix}\n{codized(page)}" for page in members_pages]
         if flags.page not in range(1, len(members_pages) + 1):
-            await ctx.reply(f"That page does not exist. Please specify page between 1 and {len(members_pages)}.")
+            await ctx.reply(
+                f"That page does not exist. Please specify page between 1 and {len(members_pages)}."
+            )
             return
         paginator = Paginator(pages=members_pages)
         paginator.current_page = flags.page - 1
