72ebd1f81ea683fdbe1922d6af75c7f7025d4977
HKGx <19597269+HKGx@users.noreply.github.com>
2022-03-10 00:42:10 +0100

    add utility cog for mentioning channels

--- lambo/cogs/strata/__init__.py
+++ lambo/cogs/strata/__init__.py
@@ -1,6 +1,8 @@
 from lambo import CustomClient
 from .valentines import setup as valentines_setup
+from .mention_channel import setup as mention_channel_setup
 
 
 def setup(bot: CustomClient):
     valentines_setup(bot)
+    mention_channel_setup(bot)
---
+++ lambo/cogs/strata/mention_channel.py
@@ -0,0 +1,49 @@
+import re
+import discord
+from discord.ext.commands import Cog, Context, command
+from discord.utils import escape_markdown
+from lambo import CustomClient
+from lambo.cogs.utilities import get_guild
+
+
+class MentionChannelCog(Cog, name="Channel Mentioning"):
+    MENTION_REGEX = re.compile(r"#(\w+)")
+    LOOKUP_GUILD_ID = 211261411119202305
+    ALLOWED_GUILD_IDS = [950868480041824266]
+
+    @property
+    def lookup_guild(self) -> discord.Guild:
+        return get_guild(self.bot, self.LOOKUP_GUILD_ID)
+
+    bot: CustomClient
+
+    def __init__(self, bot: CustomClient) -> None:
+        self.bot = bot
+
+    @Cog.listener("on_message")
+    async def on_message(self, message: discord.Message):
+        if message.guild is not None and message.guild.id not in self.ALLOWED_GUILD_IDS:
+            return
+        partial_names = self.MENTION_REGEX.findall(message.content)
+        if not partial_names:
+            return
+        channel_mentions: list[str] = []
+        for partial_name in partial_names:
+            channel_mentions += [
+                channel.mention
+                for channel in reversed(
+                    self.lookup_guild.channels
+                )  # reverse to get channel with highest position
+                if partial_name in channel.name
+            ][
+                :3
+            ]  # limit to 3 channels
+        if not channel_mentions:
+            return
+        await message.reply(
+            " ".join(channel_mentions), allowed_mentions=discord.AllowedMentions.none()
+        )
+
+
+def setup(bot: CustomClient):
+    bot.add_cog(MentionChannelCog(bot))
