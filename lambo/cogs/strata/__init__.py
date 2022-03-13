3f77bf0dda8f1822be86247ec48b6b722f182255
HKGx <19597269+HKGx@users.noreply.github.com>
2022-03-13 15:06:48 +0100

    forcefully disable valentines cog

--- lambo/cogs/strata/__init__.py
+++ lambo/cogs/strata/__init__.py
@@ -1,8 +1,9 @@
 from lambo import CustomClient
-from .valentines import setup as valentines_setup
+
+# from .valentines import setup as valentines_setup
 from .mention_channel import setup as mention_channel_setup
 
 
 def setup(bot: CustomClient):
-    valentines_setup(bot)
+    # valentines_setup(bot)
     mention_channel_setup(bot)
