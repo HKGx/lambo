540ed2c75c99df3bb7f2601a89b55a2ba6549c88
HKGx <19597269+HKGx@users.noreply.github.com>
2022-03-30 01:42:30 +0200

    increase the max length of prize in giveaway model

--- lambo/models/giveway_model.py
+++ lambo/models/giveway_model.py
@@ -6,6 +6,6 @@ class GiveawayModel(Model):
     message_id = fields.CharField(max_length=22)  # message id
     channel_id = fields.CharField(max_length=22)  # channel id
     ends_at = fields.DatetimeField()
-    prize = fields.CharField(max_length=128)
+    prize = fields.CharField(max_length=512)
     winners = fields.IntField()
     ended = fields.BooleanField(default=False)
