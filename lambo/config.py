d7d52cd265fa5b1d81c6683a98bf6a7c3b2009ab
HKGx <19597269+HKGx@users.noreply.github.com>
2022-03-30 01:20:02 +0200

    refactor config a bit

--- lambo/config.py
+++ lambo/config.py
@@ -1,20 +1,30 @@
-from typing import Optional
+from typing import Callable, Optional
 
-from pydantic import BaseSettings
+from pydantic import BaseSettings, Field
+
+
+def default_list(*l: str) -> Callable[[], list[str]]:
+    return lambda: list(l)
 
 
 class Settings(BaseSettings):
     prefix: str = "b!"
     db_url: str = "sqlite://:memory:"
-    extensions: list[str] = [
-        "lambo.cogs.count_emoji",
-        "lambo.cogs.moderation_utils",
-        "lambo.cogs.utilities",
-    ]
-    models: list[str] = ["lambo.models.used_emoji_model"]
-    non_default_extensions: list[str] = []
+    extensions: list[str] = Field(default_factory=default_list("lambo.cogs"))
+    models: list[str] = Field(default_factory=default_list("lambo.models"))
+    non_default_extensions: list[str] = Field(default_factory=list)
+    non_default_models: list[str] = Field(default_factory=list)
     token: Optional[str] = None
-    intents: Optional[list[str]] = None
+    intents: list[str] = Field(
+        default_factory=default_list(
+            "emojis",
+            "reactions",
+            "guild_messages",
+            "guilds",
+            "members",
+            "message_content",
+        )
+    )
 
     class Config:
         env_file = ".env"
